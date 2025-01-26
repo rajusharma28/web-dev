8c668b202596ff92ae879d6133759637c9e68571
Raju sharma <rs65142023@gmail.com>
2025-01-26 21:40:19 +0530

    GIT and GITHUB

---
+++ Git and Github/04Gits/first.js
@@ -0,0 +1,29 @@
+const name = "ROhit";
+const age = 23;
+
+let balance = 420;
+
+function updateBalance(value){
+    balance+=value;
+}
+
+
+console.log("Hello Coders");
+
+console.log("Hello Ji");
+
+
+const c = 20+10;
+console.log(c);
+
+function balancechecker(){
+    // 
+    console.log("UPdate");
+    // 
+}
+
+
+//  code likho
+//  5 member code likh rhe hai
+// update code: 
+// 3 line of code
---
+++ Git and Github/GitsNew 2/first.js
@@ -0,0 +1,27 @@
+const name = "Rohit";
+const age = 90;
+const balance = 300;
+
+console.log("Hello");
+function updateBalance(){
+    balance+=20;
+}
+
+updateBalance();
+
+console.log(balance);
+
+console.log("Money honey");
+console.log("Bug final commit");
+console.log("Mein hu hero tera");
+
+function showage(val){
+    console.log(age+val);
+}
+
+showage(10);
+function showName(){
+    console.log(name);
+}
+
+showName();
