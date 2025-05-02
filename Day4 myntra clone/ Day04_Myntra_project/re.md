54721513fb5b93987dc843cfaa3699cdd092752e
Raju sharma <rs65142023@gmail.com>
2025-05-02 22:17:00 +0530

    react

---
+++ Day4 myntra clone/ Day04_Myntra_project/re.md
@@ -0,0 +1,8 @@
+module->components same name 
+
+one functio export one time and than export file name used it
+
+//imp
+
+multiple fun export ke liya 
+import {fun1 as goa ->( fun name ->) rename,fun2} from file name
---
+++ Day4 myntra clone/ Day04_Myntra_project/src/utils/dummy.js
@@ -0,0 +1,20 @@
+const arr = [
+    { cloth: "T-shirt", offer: "20-40% off",price:200 },
+    { cloth: "Jeans", offer: "30-50% off",  price:300},
+    { cloth: "Jacket", offer: "40% off",price:290  },
+    { cloth: "Sneakers", offer: "25-60% off",price:490   },
+    { cloth: "Formal Shirt", offer: "35% off", price:290 },
+    { cloth: "Hoodie", offer: "20-45% off",  price:290 },
+    { cloth: "Jeans", offer: "30-50% off", price:290  },
+    { cloth: "Jeans", offer: "30-50% off", price:390  },
+    { cloth: "Jeans", offer: "30-50% off", price:2930  },
+    { cloth: "Jacket", offer: "40% off", price:2390 },
+    { cloth: "Sneakers", offer: "25-60% off", price:2930  },
+    { cloth: "Formal Shirt", offer: "35% off", price:2920 },
+    { cloth: "Hoodie", offer: "20-45% off", price:2910  },
+    
+    
+    { cloth: "Hoodie", offer: "20-45% off", price:2910  }
+];
+
+export default arr;
