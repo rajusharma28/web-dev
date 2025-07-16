54279bbe95a4cff7c369e26372bdb2657e0408e9
Raju sharma <rs65142023@gmail.com>
2025-07-16 13:33:54 +0530

    Delete backend directory

--- backend/8 middleware/fontend.js
+++
@@ -1,8 +0,0 @@
-// fetch("http://sjfdosjfio.com");
-// const response = await fetch('https://api.example.com/data',{
-//     method:'POST',
-//     headers:{
-//         'Content-Type':'application/json'
-//     },
-//     body:JSON.stringify({name:'json',age:30})
-// })
