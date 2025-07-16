4f0b1afde4701a05eb1d09799d70843b3ca8da2f
Raju sharma <rs65142023@gmail.com>
2025-07-16 13:37:49 +0530

    first

---
+++ first/index.js
@@ -0,0 +1,13 @@
+const sum=require("./first");
+const sub=require("./sub");
+const mul=require("./mul");
+
+
+// sum(2,3);
+// mul(2,3);
+// sub(2,3);
+
+module.exports={sum,sub,mul};
+// console.log("hello");
+
+
